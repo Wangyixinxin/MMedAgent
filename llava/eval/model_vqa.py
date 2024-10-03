c5b3a1e77cb5410a95f678e86a9672d7dbcf41db
andy0207 <andy0207@stanford.edu>
2024-10-02 21:29:34 -0700

    eval_vaq

--- llava/eval/model_vqa.py
+++ llava/eval/model_vqa.py
@@ -9,8 +9,7 @@ from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_S
 from llava.conversation import conv_templates, SeparatorStyle
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
-from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
-
+from llava.mm_utils import tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria, process_images
 from PIL import Image
 import math
 
@@ -40,11 +39,35 @@ def eval_model(args):
     ans_file = open(answers_file, "w")
     for line in tqdm(questions):
         idx = line["question_id"]
-        image_file = line["image"]
-        if "pre_text" in line:
-            qs_pre = line["pre_text"]
-            assist = line["assist"]
+        if 'image' in line:
+            image_file = line["image"]
+        else:
+            image_file = None
+        if 'pre_text' in line:
+            qs_pre = line['pre_text']
+            assist = line['assist']
+        else:
+            qs_pre = None
         qs = line["text"]
+        if "gpt4_answer" in line:
+            gpt4_answer = line['gpt4_answer']
+        else:
+            gpt4_answer = ""
+        if "in_text_mention" in line:
+            in_text_mention = line["in_text_mention"]
+        else:
+            in_text_mention = ""
+        if "domain" in line:
+            domain = line["domain"]
+        else:
+            domain = ""
+        if "fig_caption" in line:
+            fig_caption = line["fig_caption"]
+        else:
+            fig_caption = ""
+        conv_type = line['type'] if 'type' in line else 'conv'
+        if 'pre_text' in line:
+            qs = line["pre_text"]
         cur_prompt = qs
         if model.config.mm_use_im_start_end:
             qs = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs
@@ -59,27 +82,45 @@ def eval_model(args):
         conv.append_message(conv.roles[1], None)
         prompt = conv.get_prompt()
 
-        input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
-
-        image = Image.open(os.path.join(args.image_folder, image_file))
-        image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-
+        # input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+        if image_file is not None:
+            input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
+            image = Image.open(os.path.join(args.image_folder, image_file))
+            image_tensor = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            image_tensor = image_tensor.unsqueeze(0).half().cuda()
+        else:
+            # image_tensor = torch.zeros(1,3,224,224).half().cuda()
+            inputs = tokenizer([prompt])
+            input_ids = torch.as_tensor(inputs.input_ids).cuda()
+            image_tensor = None
         stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
         keywords = [stop_str]
         stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
-
-        with torch.inference_mode():
-            output_ids = model.generate(
-                input_ids,
-                images=image_tensor.unsqueeze(0).half().cuda(),
-                do_sample=True if args.temperature > 0 else False,
-                temperature=args.temperature,
-                top_p=args.top_p,
-                num_beams=args.num_beams,
-                # no_repeat_ngram_size=3,
-                max_new_tokens=1024,
-                use_cache=True)
-
+        if image_tensor is not None:
+            with torch.inference_mode():
+                output_ids = model.generate(
+                    input_ids,
+                    images=image_tensor,
+                    do_sample=True if args.temperature > 0 else False,
+                    temperature=args.temperature,
+                    top_p=args.top_p,
+                    num_beams=args.num_beams,
+                    # no_repeat_ngram_size=3,
+                    max_new_tokens=1024,
+                    use_cache=True,
+                )
+        else:
+            with torch.inference_mode():
+                output_ids = model.generate(
+                    input_ids,
+                    do_sample=True if args.temperature > 0 else False,
+                    temperature=args.temperature,
+                    top_p=args.top_p,
+                    num_beams=args.num_beams,
+                    # no_repeat_ngram_size=3,
+                    max_new_tokens=1024,
+                    use_cache=True,
+                )
         input_token_len = input_ids.shape[1]
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
         if n_diff_input_output > 0:
@@ -88,13 +129,22 @@ def eval_model(args):
         outputs = outputs.strip()
         if outputs.endswith(stop_str):
             outputs = outputs[:-len(stop_str)]
+            
         outputs = outputs.strip()
 
+        
+        
         ans_id = shortuuid.uuid()
         ans_file.write(json.dumps({"question_id": idx,
+                                   "image": image_file,
                                    "prompt": cur_prompt,
                                    "text": outputs,
+                                   "gpt4_answer": gpt4_answer,
+                                   "in_text_mention": in_text_mention,
+                                   "domain": domain,
+                                   "fig_caption": fig_caption,
                                    "answer_id": ans_id,
+                                   "type": conv_type,
                                    "model_id": model_name,
                                    "metadata": {}}) + "\n")
         ans_file.flush()
